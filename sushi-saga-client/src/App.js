c1f53413f244fd283d230ed141def27484695ff2
NMoore42 <nicholas.moore5@icloud.com>
2019-06-11 10:55:15 -0500

    added endless scroll

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -12,7 +12,8 @@ class App extends Component {
       sushi: [],
       converyorStart: 0,
       converyorEnd: 4,
-      moneyLeft: 100
+      moneyLeft: 100,
+      emptyPlates: []
     }
   }
 
@@ -26,29 +27,32 @@ class App extends Component {
   eatSushiHandler = (id) => {
     const clickedSushi = this.state.sushi.find(sushi => sushi.id === id)
     const clickedSushiIndex = this.state.sushi.findIndex(sushi => sushi === clickedSushi)
-    if (this.state.moneyLeft - clickedSushi.price >= 0) {
-      const newSushi = this.state.sushi.slice()
-      newSushi[clickedSushiIndex] = {
-        id: clickedSushi.id,
-        name: clickedSushi.name,
-        img_url: clickedSushi.img_url,
-        price: clickedSushi.price,
-        created_at: clickedSushi.created_at,
-        eaten: true
+    if (!clickedSushi.eaten) {
+      if (this.state.moneyLeft - clickedSushi.price >= 0) {
+        const newSushi = this.state.sushi.slice()
+        newSushi[clickedSushiIndex] = {
+          id: clickedSushi.id,
+          name: clickedSushi.name,
+          img_url: clickedSushi.img_url,
+          price: clickedSushi.price,
+          created_at: clickedSushi.created_at,
+          eaten: true,
+          emptyPlates: this.state.emptyPlates.push("empty")
+        }
+        this.setState({
+          sushi: newSushi,
+          moneyLeft: this.state.moneyLeft - clickedSushi.price
+        })
+      } else {
+        alert("You don't have enough money, ya bum!")
       }
-      this.setState({
-        sushi: newSushi,
-        moneyLeft: this.state.moneyLeft - clickedSushi.price
-      })
-    } else {
-      alert("You don't have enough money, ya bum!")
     }
   }
 
   moreSushiHanlder = () => {
     this.setState({
-      converyorStart: this.state.converyorStart + 4,
-      converyorEnd: this.state.converyorEnd + 4
+      converyorStart: this.state.converyorStart !== 96 ? this.state.converyorStart + 4 : 0,
+      converyorEnd: this.state.converyorEnd !== 100 ? this.state.converyorEnd + 4 : 4
     })
   }
 
@@ -64,7 +68,7 @@ class App extends Component {
     return (
       <div className="app">
         <SushiContainer  sushi={this.displaySushi()} eatSushiHandler={this.eatSushiHandler} moreSushiHanlder={this.moreSushiHanlder}/>
-        <Table moneyLeft={this.state.moneyLeft}/>
+        <Table moneyLeft={this.state.moneyLeft} emptyPlates={this.state.emptyPlates}/>
       </div>
     );
   }
